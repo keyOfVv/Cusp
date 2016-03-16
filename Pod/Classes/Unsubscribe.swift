07d8f1bc80ab3e9fb8a932467baf359aa6b5495a
Ke Yang <ofveravi@gmail.com>
2016-03-16 13:56:13 +0800

    modified comments

--- Pod/Classes/Unsubscribe.swift
+++ Pod/Classes/Unsubscribe.swift
@@ -28,7 +28,6 @@ internal class UnsubscribeRequest: PeripheralOperationRequest {
 	Convenient initializer
 
 	- parameter characteristic: a CBCharacteristic object of which the value update to be unsubscribed
-	- parameter peripheral:     a CBPeripheral object to which the characteristic belongs
 	- parameter success:        a closure called when unsubscription succeed
 	- parameter failure:        a closure called when unsubscription failed
 
@@ -42,8 +41,7 @@ internal class UnsubscribeRequest: PeripheralOperationRequest {
 	}
 
 	override internal var hash: Int {
-		let string = self.characteristic.UUID.UUIDString
-		return string.hashValue
+		return characteristic.UUID.UUIDString.hashValue
 	}
 
 	override internal func isEqual(object: AnyObject?) -> Bool {
@@ -62,7 +60,6 @@ extension Peripheral {
 	取消订阅指定从设备的指定特征的数值变化
 
 	- parameter characteristic: a CBCharacteristic object of which the value update to be unsubscribed. 待取消订阅数值更新的特征.
-	- parameter peripheral:     a CBPeripheral object to which the characteristic belongs. 特征所属的从设备.
 	- parameter success:        a closure called when unsubscription succeed. 取消订阅成功时执行的闭包.
 	- parameter failure:        a closure called when unsubscription failed. 取消订阅失败时执行的闭包.
 	*/
@@ -72,22 +69,24 @@ extension Peripheral {
 			failure?(error)
 			return
 		}
-
+		// 1. create req
 		let req = UnsubscribeRequest(characteristic: characteristic, success: success, failure: failure)
+		// 2. add req
 		dispatch_async(self.requestQ, { () -> Void in
 			self.unsubscribeRequests.insert(req)
 		})
-
+		// 3. unsubscribe characteristic
 		dispatch_async(self.operationQ, { () -> Void in
 			self.core.setNotifyValue(false, forCharacteristic: characteristic)
 		})
-
+		// 4. set time out closure
 		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(req.timeoutPeriod * Double(NSEC_PER_SEC))), self.operationQ) { () -> Void in
 			if req.timedOut {
 				dispatch_async(dispatch_get_main_queue(), { () -> Void in
 					let error = NSError(domain: "connect operation timed out", code: Cusp.Error.TimedOut.rawValue, userInfo: nil)
 					failure?(error)
 				})
+				// since req timed out, don't need it any more
 				dispatch_async(self.requestQ, { () -> Void in
 					self.unsubscribeRequests.remove(req)
 				})
