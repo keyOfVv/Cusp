60fc9e9460c395e84558d69be0eee91eadffbf70
Ke Yang <ofveravi@gmail.com>
2016-03-16 13:59:18 +0800

    modified comments

--- Pod/Classes/RSSI.swift
+++ Pod/Classes/RSSI.swift
@@ -60,28 +60,30 @@ extension Peripheral {
 	/// - parameter success: 获取成功的回调;
 	/// - parameter failure: 获取失败的回调;
 	/// - parameter timedOut: 获取超时的回调;
-	public func readRSSI(peripheral: Peripheral, success: ((Response?) -> Void)?, failure: ((NSError?) -> Void)?) {
+	public func readRSSI(success success: ((Response?) -> Void)?, failure: ((NSError?) -> Void)?) {
 		// 0. check if ble is available
 		if let error = Cusp.central.assertAvailability() {
 			failure?(error)
 			return
 		}
-
+		// 1. create req
 		let req = RSSIRequest(success: success, failure: failure)
+		// 2. add req
 		dispatch_async(self.requestQ, { () -> Void in
 			self.RSSIRequests.insert(req)
 		})
-
+		// 3. read RSSI
 		dispatch_async(self.operationQ, { () -> Void in
-			peripheral.core.readRSSI()
+			self.core.readRSSI()
 		})
-
+		// 4. set time out closure
 		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(req.timeoutPeriod * Double(NSEC_PER_SEC))), self.operationQ) { () -> Void in
 			if req.timedOut {
 				dispatch_async(dispatch_get_main_queue(), { () -> Void in
 					let error = NSError(domain: "connect operation timed out", code: Cusp.Error.TimedOut.rawValue, userInfo: nil)
 					failure?(error)
 				})
+				// since req timed out, don't need it any more
 				dispatch_async(self.requestQ, { () -> Void in
 					self.RSSIRequests.remove(req)
 				})
