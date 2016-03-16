09c72126370ed699285577a61acd4318a7c256ce
Ke Yang <ofveravi@gmail.com>
2016-03-16 18:46:38 +0800

    added new class Subscription
    Subscription has a property named update, it’s a closure.

---
+++ Pod/Classes/Subscription.swift
@@ -0,0 +1,35 @@
+//
+//  Subscription.swift
+//  Pods
+//
+//  Created by Ke Yang on 3/16/16.
+//
+//
+
+import Foundation
+
+internal class Subscription: NSObject {
+	internal private(set) var characteristic: Characteristic!
+	internal private(set) var update: ((Response) -> Void)?
+
+	private override init() {
+		super.init()
+	}
+
+	convenience init(characteristic: Characteristic, update: ((Response) -> Void)?) {
+		self.init()
+		self.characteristic = characteristic
+		self.update = update
+	}
+
+	override var hash: Int {
+		return self.characteristic.hashValue
+	}
+
+	override func isEqual(object: AnyObject?) -> Bool {
+		if let other = object as? Subscription {
+			return self.hashValue == other.hashValue
+		}
+		return false
+	}
+}
