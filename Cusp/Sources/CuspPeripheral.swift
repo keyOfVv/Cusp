cbf9802d0a5f16fff147b0b309b9fff92c5a312e
Ke Yang <ofveravi@gmail.com>
2017-01-16 18:47:35 +0800

    n/a

---
+++ Cusp/Sources/CuspPeripheral.swift
@@ -0,0 +1,13 @@
+//
+//  CuspPeripheral.swift
+//  Cusp
+//
+//  Created by Ke Yang on 16/01/2017.
+//  Copyright © 2017 com.keyofvv. All rights reserved.
+//
+
+import UIKit
+
+class CuspPeripheral: NSObject {
+
+}
